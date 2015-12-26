b609782a0f868634596baf28c7b7b4aee79ef2d8
webprofusion-chrisc <christopher.cook@webprofusion.com>
2015-12-26 11:19:28 +0800

    Work in progress - ionic v2 app

---
+++ App/Ionic2/testapp/www/app/page1/page1.ts
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-framework/ionic';
+
+
+@Page({
+  templateUrl: 'app/page1/page1.html',
+})
+export class Page1 {
+  constructor() {
+
+  }
+}
---
+++ App/Ionic2/testapp/www/app/page3/page3.ts
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-framework/ionic';
+
+
+@Page({
+  templateUrl: 'app/page3/page3.html'
+})
+export class Page3 {
+  constructor() {
+
+  }
+}
